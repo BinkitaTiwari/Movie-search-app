f11883b820410936e2f7d7838c3bbe94a74dd56d
Binkita Tiwari <135starmoon@gmail.com>
2020-08-11 20:19:18 +0530

    app is working, some problem in UI

---
+++ src/Components/Search/Search.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './Search.css';
+
+const Search = (props) => {
+    let resultList = null
+
+    if (props.searching && (props.defaultTitle !== '')) {
+        resultList = (
+            <ul className="results">
+                {props.results.map(item => (
+                    <li key={item.imdbID} onClick={() => props.clicked(item)}>
+                        <img src={item.Poster} alt="Movie Poster"/>
+                        {item.Title}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
+    return (
+        <div className="search">
+            <input type="text" name="movie-search" value ={props.defaultTitle} onChange={props.search} placeholder="Enter Your movie" />
+            {resultList}
+        </div>
+    );
+};
+
+export default Search;
---
+++ src/MovieSearch.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import MovieCard from './Components/Movie-card/Movie-card';
+import Search from './Components/Search/Search';
+
+class MovieSearch extends Component {
+    state = {
+        movieId: '', // default imdb id (Spartacus)
+        title: '',
+        movie: {},
+        searchResults: [],
+        isSearching: false,
+    }
+
+    componentDidMount() {
+        this.loadMovie()
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.movieId !== this.state.movieId) {
+            this.loadMovie()
+        }
+        
+    }
+
+    loadMovie() {
+        axios.get(`http://www.omdbapi.com/?apikey=d207ea3b&i=${this.state.movieId}`)
+            .then(response => {
+                this.setState({ movie: response.data });
+            })
+            .catch(error => {
+                console.log('Opps!', error.message);
+            })
+    }
+
+    // we use a timeout to prevent the api request to fire immediately as we type
+    timeout = null;
+
+    searchMovie = (event) => {
+        this.setState({ title: event.target.value, isSearching: true })
+
+        clearTimeout(this.timeout);
+
+        this.timeout = setTimeout(() => {
+            axios.get(`http://www.omdbapi.com/?apikey=d207ea3b&s=${this.state.title}`)
+                .then(response => {
+
+                    if (response.data.Search) {
+                        const movies = response.data.Search.slice(0, 5);
+                        this.setState({ searchResults: movies });
+                    }
+                })
+                .catch(error => {
+                    console.log('Opps!', error.message);
+                })
+        }, 1000)
+
+
+    }
+
+    // event handler for a search result item that is clicked
+    itemClicked = (item) => {
+        this.setState(
+            {
+                movieId: item.imdbID,
+                isSearching: false,
+                title: item.Title,
+            }
+        )
+    }
+
+
+    render() {
+        return (
+            <div onClick={() => this.setState({ isSearching: false })}>
+                <Search
+                    defaultTitle={this.state.title}
+                    search={this.searchMovie}
+                    results={this.state.searchResults}
+                    clicked={this.itemClicked}
+                    searching={this.state.isSearching} />
+
+                {this.state.movieId? <MovieCard movie={this.state.movie}/> : null}
+            </div>
+        );
+    }
+}
+
+export default MovieSearch;
